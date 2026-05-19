22ab171e3edadca288388536d64f95652ae67ec2
sumjitg <photographyworld93@gmail.com>
2026-05-20 00:53:28 +0530

    fix: standardized TanStack Start Vercel deployment with Vinxi and Nitro presets

--- app.config.ts
+++ app.config.ts
@@ -3,27 +3,12 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default createApp({
-  routers: [
-    {
-      name: "public",
-      type: "static",
-      dir: "./public",
-      base: "/",
-    },
-    {
-      name: "client",
-      type: "client",
-      handler: "./src/entry-client.tsx",
-      target: "browser",
-      base: "/_build",
-    },
-    {
-      name: "ssr",
-      type: "http",
-      handler: "./src/entry-server.tsx",
-      target: "server",
-      base: "/",
-    },
+  plugins: [
+    tsconfigPaths(),
+    tanstackStart({
+      server: {
+        entry: "./src/server.ts",
+      },
+    }),
   ],
-  plugins: [tsconfigPaths(), tanstackStart()],
 });
